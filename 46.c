f19fefdc02a6d5eaeb535920f8f20a0066df4914
Hardik082021 <49453368+Hardik082021@users.noreply.github.com>
2019-04-16 23:49:14 +0530

    Add files via upload

--- 46.c
+++ 46.c
@@ -39,4 +39,30 @@ p1_list[r] = p;
 
 }
  } //to sort the process so the process with minimum arrival time 
- 
+ int main()
+ {
+
+int i,t=0,b_t=0,peak;
+
+int a[10];
+
+float wait_time = 0, taround_time = 0, avg_w_t1=0, avg_taround_time1=0;
+
+printf("enter the no. of processes: ");
+
+scanf("%d",&n);
+
+for(i = 0; i < n; i++)
+
+{
+
+p1_list[i].p_no = i+1;
+
+printf("\nEnter Details For P%d process:-\n", p1_list[i].p_no);
+printf("Enter Arrival Time: ");
+scanf("%d", &p1_list[i].arrival_time );
+printf("Enter Burst Time: ");
+scanf("%d", &p1_list[i].burst_time);
+p1_list[i].flag = 0;
+b_t = b_t + p1_list[i].burst_time;
+}
